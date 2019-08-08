e8c3c9fe659108c414fee67887ba65171f0f2f06
polyphant1 <chrisl1991@hotmail.co.uk>
2019-08-08 14:58:16 +0100

    fixed to choose overwrite

--- cutout.py
+++ cutout.py
@@ -4,8 +4,10 @@ import eagle as E
 import h5py
 import flares
 
-from h5py_utilities import check_h5py, write_data_h5py, create_group_h5py
+from h5py_utilities import check_h5py, write_data_h5py, create_group_h5py, load_h5py
 
+overwrite = False
+verbose = True
 
 fl = flares.flares()
 centre = [3200./2,3200./2,3200./2]
@@ -18,39 +20,50 @@ fname = 'data/flares_%d.h5'
 #     del f['radius']
 
 
-r = {h: {t: 0. for t in fl.tags} for h in fl.halos}
-fl.save_dict_to_hdf5(dic=r,filename=fname,groupname='radius')
+# r = {h: {t: 0. for t in fl.tags} for h in fl.halos}
+# fl.save_dict_to_hdf5(dic=r,filename=fname,groupname='radius')
 
 
+create_group_h5py(fname,'radius')
 create_group_h5py(fname,'masks')
 
 for halo in fl.halos:
     
-    print("Halo: %s"%halo)
+    if verbose: print("Halo: %s"%halo)
     halo_dir = fl.directory + 'geagle_' + halo + '/data/' 
 
     create_group_h5py(fname, 'masks/%s'%halo)
+    create_group_h5py(fname, 'radius/%s'%halo)
 
-    if fl.check_snap_exists(halo, fl.tags[1]): tags = fl.tags
-    else: tags = fl.alt_tags
+    # if fl.check_snap_exists(halo, fl.tags[1]): tags = fl.tags
+    # else: tags = fl.alt_tags
+    tags = fl.tags
 
     for rtag,tag in zip(fl.tags,tags):
+        if verbose: print("Loading data...",rtag)
 
-        # print("Loading data...",rtag,tag)
-        # dm_cood = E.readArray('SNAPSHOT', halo_dir, tag, "/PartType1/Coordinates", physicalUnits=False, noH=True, numThreads=1)
-        cop = E.readArray("SUBFIND", halo_dir, tag, "/Subhalo/CentreOfPotential", numThreads=1, noH=True, physicalUnits=False)
-
-        # r[halo][rtag] = fl.cut_radius(dm_cood[:,0],dm_cood[:,1],dm_cood[:,2],3200/2,3200/2,3200/2, threshold = 0.6, cut = 1.5)
-        r[halo][rtag] = fl.cut_radius(cop[:,0],cop[:,1],cop[:,2],
-                                      3200/2,3200/2,3200/2, 
-                                      threshold = 0.6, cut = 1.)
+        if (check_h5py(fname, 'radius/%s/%s'%(halo,tag)) is False) |\
+            (overwrite is True):
+        
+            cop = E.readArray("SUBFIND", halo_dir, tag, 
+                              "/Subhalo/CentreOfPotential", numThreads=1, 
+                              noH=True, physicalUnits=False)
+    
+            r = fl.cut_radius(cop[:,0],cop[:,1],cop[:,2],
+                              3200/2,3200/2,3200/2, 
+                              threshold = 0.6, cut = 1.)
    
-        fl.save_dict_to_hdf5(r,fname,'radius', overwrite=True)
-        
-        mask = fl.cutout(cop,centre,r[halo][rtag])
-        
-        gstr = 'masks/%s'%halo
-        write_data_h5py(fname, gstr, rtag, data=mask, overwrite=True)
-        print(rtag, r[halo][rtag])
+            gstr = 'radius/%s'%halo
+            write_data_h5py(fname,gstr,rtag,data=r, overwrite=True)
+            
+            mask = fl.cutout(cop,centre,r)
+            
+            gstr = 'masks/%s'%halo
+            write_data_h5py(fname,gstr,rtag,data=mask, overwrite=True)
+            
+            print(rtag, r)
 
-
+        else:
+            r = load_h5py(fname, 'radius/%s/%s'%(halo,tag))
+            if verbose: print('Already exists (r=%d), overwrite=False'%r)
+        
