4816fd3513609c4774b8ff8fe42fed0a829393b9
polyphant1 <chrisl1991@hotmail.co.uk>
2019-08-08 14:58:50 +0100

    fixed overwrite, tidy

--- download.py
+++ download.py
@@ -1,59 +1,64 @@
 import sys
-
 import numpy as np
-# import eagle as E
-import eagle_IO as E
 
+from eagle_IO import eagle_IO as E
 import flares
 from h5py_utilities import write_data_h5py, create_group_h5py, check_h5py
+
+overwrite = False
 
 fl = flares.flares()
 fname = 'data/flares_%d.h5'
 
 create_group_h5py(fname, 'mstar')
 create_group_h5py(fname, 'sfr')
-# create_group_h5py(fname, 'coods')
+create_group_h5py(fname, 'centrals')
 
 for halo in fl.halos:
+    
+    print(halo)
 
     create_group_h5py(fname, 'mstar/%s'%halo)
     create_group_h5py(fname, 'sfr/%s'%halo)
     create_group_h5py(fname, 'centrals/%s'%halo)
 
-    # if fl.check_snap_exists(halo, fl.tags[1]): tags = fl.tags
-    # else: tags = fl.alt_tags
-    tags = fl.tags
+    if halo in ['0014']: overwrite = True
+    else: overwrite = False
 
-    for tag,rtag in zip(tags,fl.tags):
 
-        print(halo, tag)
+    for tag in fl.tags:
+
         halodir = fl.directory+'/geagle_'+halo+'/data/'
 
-
-        if check_h5py(fname, 'mstar/%s/%s'%(halo,tag)) is False:
-            mstar = E.read_array("SUBFIND", halodir, tag, "/Subhalo/Stars/Mass", 
-                                numThreads=1, noH=True)
-            write_data_h5py(fname, 'mstar/%s'%halo, rtag, data=mstar, overwrite=True)
+        if (check_h5py(fname, 'mstar/%s/%s'%(halo,tag)) is False) |\
+                (overwrite == True):
             
-        if check_h5py(fname, 'sfr/%s/%s'%(halo,tag)) is False:
-            sfr = E.read_array("SUBFIND", halodir, tag, "/Subhalo/StarFormationRate", 
-                              numThreads=1, noH=True)
-            write_data_h5py(fname, 'sfr/%s'%halo, rtag, data=sfr, overwrite=True)
+            mstar = E.read_array("SUBFIND", halodir, tag, 
+                                 "/Subhalo/Stars/Mass", 
+                                 numThreads=1, noH=True)
 
-        if check_h5py(fname, 'centrals/%s/%s'%(halo,tag)) is False:
-            centrals = (E.read_array("SUBFIND", halodir, tag, "/Subhalo/SubGroupNumber", 
-                                    numThreads=1, noH=True) == 0)
-            write_data_h5py(fname, 'centrals/%s'%halo, rtag, data=centrals, overwrite=True)
+            write_data_h5py(fname, 'mstar/%s'%halo, tag, 
+                            data=mstar, overwrite=True)
+           
+
+        if (check_h5py(fname, 'sfr/%s/%s'%(halo,tag)) is False) |\
+                (overwrite == True):
             
+            sfr = E.read_array("SUBFIND", halodir, tag, 
+                               "/Subhalo/StarFormationRate", 
+                               numThreads=1, noH=True)
 
-        # coods[halo][tag] = E.readArray("SUBFIND", halodir, tag, "/Subhalo/CentreOfPotential", numThreads=1, noH=True, physicalUnits=False)
-        # bhmass[halo][tag] = E.readArray("SUBFIND", halodir, tag, "/Subhalo/BlackHoleMass", numThreads=1, noH=True)
-        # tmass[halo][tag] = E.readArray("SUBFIND", halodir, tag, "/Subhalo/Mass", numThreads=1, noH=True)
-        # Zstars[halo][tag] = E.readArray("SUBFIND", halodir, tag, "/Subhalo/Stars/Metallicity", numThreads=1, noH=True)
-        # ZgasSF[halo][tag] = E.readArray("SUBFIND", halodir, tag, "/Subhalo/SF/Metallicity", numThreads=1, noH=True)
-        # grpn = E.readArray("SUBFIND", halodir, tag, "/Subhalo/GroupNumber", numThreads=1)
-        # m200[halo][tag] = E.readArray("SUBFIND_GROUP", halodir, tag, "/FOF/Group_M_Mean200", numThreads=1, noH=True)[grpn - 1]
-    #     bhaccr[halo][tag] = E.readArray("SUBFIND", halodir, tag, "/Subhalo/BlackHoleMassAccretionRate", numThreads=1, noH=True)
+            write_data_h5py(fname, 'sfr/%s'%halo, tag, 
+                            data=sfr, overwrite=True)
 
-    #     ZgasNSF[halo][tag] = E.readArray("SUBFIND", halodir, tag, "/Subhalo/NSF/Metallicity", numThreads=1, noH=True)
 
+        if (check_h5py(fname, 'centrals/%s/%s'%(halo,tag)) is False) |\
+                (overwrite == True):
+
+            centrals = (E.read_array("SUBFIND", halodir, tag, 
+                                     "/Subhalo/SubGroupNumber", 
+                                     numThreads=1, noH=True) == 0)
+            
+            write_data_h5py(fname, 'centrals/%s'%halo, tag, 
+                            data=centrals, overwrite=True)
+            
