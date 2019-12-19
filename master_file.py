450952c2b9744d85f007255f9be4d220b5b76a8e
polyphant1 <chrisl1991@hotmail.co.uk>
2019-12-19 16:56:51 +0000

    updated flares module

--- master_file.py
+++ master_file.py
@@ -1,19 +1,19 @@
 import h5py
 import flares
 
-fl = flares.flares()
+fl = flares.flares('data/flares.hdf5',sim_type='FLARES')
 
 in_dir = '/cosma7/data/dp004/dc-payy1/my_files/flares_pipeline/data/'
 
-outfile = h5py.File('data/flares.h5','w')
+with h5py.File('data/flares.hdf5','w') as outfile:
 
-for halo in fl.halos:
-    print(halo)
-    
-    fl.create_group_h5py('data/flares.h5',halo)
-    
-    infile = h5py.File('%s/GEAGLE_%s_sp_info.hdf5'%(in_dir,halo),'r') 
-   
-    for tag in fl.tags:
-        infile.copy(tag,outfile[halo])
+    for halo in fl.halos:
+        print(halo)
+        
+        fl.create_group_hdf5('data/flares.hdf5',halo)
+        
+        infile = h5py.File('%s/GEAGLE_%s_sp_info.hdf5'%(in_dir,halo),'r') 
+       
+        for tag in fl.tags:
+            infile.copy(tag,outfile[halo])
 
