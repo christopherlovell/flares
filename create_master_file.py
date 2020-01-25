3b099f228abf9db66e37a25847e0f8ce2f627c20
Aswin Payyoor Vijayan <aswinpvijayan@gmail.com>
2020-01-25 04:39:47 +0000

    changed named to FLARES

--- create_master_file.py
+++ create_master_file.py
@@ -1,7 +1,7 @@
 import h5py
 import flares
 
-fl = flares.flares('./flares.hdf5',sim_type='FLARES')
+fl = flares.flares('./data/flares.hdf5',sim_type='FLARES')
 
 in_dir = '/cosma7/data/dp004/dc-payy1/my_files/flares_pipeline/data/'
 
@@ -10,7 +10,7 @@ with h5py.File('./data/flares.hdf5','w') as outfile:
     for halo in fl.halos:
         print(halo)
 
-        fl.create_group(halo)
+        fl.create_group(F'{halo}')
 
         infile = h5py.File('%s/FLARES_%s_sp_info.hdf5'%(in_dir,halo),'r')
 
